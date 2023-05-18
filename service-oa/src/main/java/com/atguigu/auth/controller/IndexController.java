b3309d999212d35be078c2a421f8ec688da068a9
HailunHou <1635928471@qq.com>
2023-05-18 22:09:22 +0800

    operate:update location:p25

---
+++ service-oa/src/main/java/com/atguigu/auth/controller/IndexController.java
@@ -0,0 +1,59 @@
+/**
+ * @author BOOM
+ * @create 2023-05-18 21:57
+ */
+package com.atguigu.auth.controller;
+
+import com.atguigu.common.result.Result;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ @author boom
+ @create 2023-05-18 21:57
+ */
+
+@Api(tags = "后台登录管理")
+@RestController
+@RequestMapping("/admin/system/index")
+public class IndexController {
+
+    /**
+     * login
+     * @return
+     */
+    @PostMapping("login")
+    public Result login() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("token","admin");
+        return Result.ok(map);
+    }
+
+    /**
+     * 获取用户信息
+     * @return
+     */
+    @GetMapping("info")
+    public Result info() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("roles","[admin]");
+        map.put("name","admin");
+        map.put("avatar","https://oss.aliyuncs.com/aliyun_id_photo_bucket/default_handsome.jpg");
+        return Result.ok(map);
+    }
+
+    /**
+     * 退出
+     * @return
+     */
+    @PostMapping("logout")
+    public Result logout(){
+        return Result.ok();
+    }
+}
