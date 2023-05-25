f06190ee8ad4506c9450a3bad5b4a7613e05a17f
HailunHou <1635928471@qq.com>
2023-05-25 20:55:09 +0800

    operate:update location:p41

---
+++ service-oa/src/main/java/com/atguigu/auth/service/impl/SysMenuServiceImpl.java
@@ -0,0 +1,113 @@
+package com.atguigu.auth.service.impl;
+
+
+import com.atguigu.auth.mapper.SysMenuMapper;
+import com.atguigu.auth.service.SysMenuService;
+import com.atguigu.auth.service.SysRoleMenuService;
+import com.atguigu.auth.utils.MenuHelper;
+import com.atguigu.common.config.exception.GuiguException;
+import com.atguigu.model.system.SysMenu;
+import com.atguigu.model.system.SysRoleMenu;
+import com.atguigu.vo.system.AssginMenuVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * <p>
+ * 菜单表 服务实现类
+ * </p>
+ *
+ * @author atguigu
+ * @since 2023-05-24
+ */
+@Service
+public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> implements SysMenuService {
+
+    @Autowired
+    private SysRoleMenuService sysRoleMenuService;
+
+    @Override
+    public List<SysMenu> findNodes() {
+        //全部权限列表
+        List<SysMenu> sysMenuList = baseMapper.selectList(null);
+        if (CollectionUtils.isEmpty(sysMenuList)) return null;
+
+        //构建树形数据
+        List<SysMenu> result = MenuHelper.buildTree(sysMenuList);
+        return result;
+    }
+
+    //删除菜单
+    @Override
+    public void removeMenuById(Long id) {
+        //判断当前菜单是否有下一层菜单
+        LambdaQueryWrapper<SysMenu> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(SysMenu::getParentId,id);
+        Integer count = baseMapper.selectCount(wrapper);
+        if(count > 0) {
+            throw new GuiguException(201,"菜单不能删除");
+        }
+        baseMapper.deleteById(id);
+    }
+
+    //查询所有菜单和角色分配的菜单
+    @Override
+    public List<SysMenu> findMenuByRoleId(Long roleId) {
+        //1 查询所有菜单- 添加条件 status=1
+        LambdaQueryWrapper<SysMenu> wrapperSysMenu = new LambdaQueryWrapper<>();
+        wrapperSysMenu.eq(SysMenu::getStatus,1);
+        List<SysMenu> allSysMenuList = baseMapper.selectList(wrapperSysMenu);
+
+        //2 根据角色id roleId查询 角色菜单关系表里面 角色id对应所有的菜单id
+        LambdaQueryWrapper<SysRoleMenu> wrapperSysRoleMenu = new LambdaQueryWrapper<>();
+        wrapperSysRoleMenu.eq(SysRoleMenu::getRoleId,roleId);
+        List<SysRoleMenu> sysRoleMenuList = sysRoleMenuService.list(wrapperSysRoleMenu);
+
+        //3 根据获取菜单id，获取对应菜单对象
+        List<Long> menuIdList = sysRoleMenuList.stream().map(c -> c.getMenuId()).collect(Collectors.toList());
+
+        //3.1 拿着菜单id 和所有菜单集合里面id进行比较，如果相同封装
+        allSysMenuList.stream().forEach(item -> {
+            if(menuIdList.contains(item.getId())) {
+                item.setSelect(true);
+            } else {
+                item.setSelect(false);
+            }
+        });
+
+        //4 返回规定树形显示格式菜单列表
+        List<SysMenu> sysMenuList = MenuHelper.buildTree(allSysMenuList);
+        return sysMenuList;
+    }
+
+    //为角色分配菜单
+    @Override
+    public void doAssign(AssginMenuVo assginMenuVo) {
+        //1 根据角色id 删除菜单角色表 分配数据
+        LambdaQueryWrapper<SysRoleMenu> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(SysRoleMenu::getRoleId,assginMenuVo.getRoleId());
+        sysRoleMenuService.remove(wrapper);
+
+        //2 从参数里面获取角色新分配菜单id列表，
+        // 进行遍历，把每个id数据添加菜单角色表
+        List<Long> menuIdList = assginMenuVo.getMenuIdList();
+        for(Long menuId:menuIdList) {
+            if(StringUtils.isEmpty(menuId)) {
+                continue;
+            }
+            SysRoleMenu sysRoleMenu = new SysRoleMenu();
+            sysRoleMenu.setMenuId(menuId);
+            sysRoleMenu.setRoleId(assginMenuVo.getRoleId());
+            sysRoleMenuService.save(sysRoleMenu);
+        }
+    }
+
+}
