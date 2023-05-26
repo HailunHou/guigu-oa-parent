fb1f2b0da983c03985f48f38bde8748650f5307b
HailunHou <1635928471@qq.com>
2023-05-26 22:17:25 +0800

    operate:update location:p46

--- service-oa/src/main/java/com/atguigu/auth/controller/IndexController.java
+++ service-oa/src/main/java/com/atguigu/auth/controller/IndexController.java
@@ -4,12 +4,17 @@
  */
 package com.atguigu.auth.controller;
 
+import com.atguigu.auth.service.SysUserService;
+import com.atguigu.common.config.exception.GuiguException;
+import com.atguigu.common.jwt.JwtHelper;
 import com.atguigu.common.result.Result;
+import com.atguigu.common.utils.MD5;
+import com.atguigu.model.system.SysUser;
+import com.atguigu.vo.system.LoginVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,14 +29,49 @@ import java.util.Map;
 @RequestMapping("/admin/system/index")
 public class IndexController {
 
+    @Autowired
+    private SysUserService sysUserService;
     /**
      * login
      * @return
      */
     @PostMapping("login")
-    public Result login() {
-        Map<String, Object> map = new HashMap<>();
-        map.put("token","admin");
+    public Result login(@RequestBody LoginVo loginVo) {
+//{"code":200,"data":{"token":"admin-token"}}
+//        Map<String,Object> map = new HashMap<>();
+//        map.put("token","admin-token");
+//        return Result.ok(map);
+        //1 获取输入用户名和密码
+        //2 根据用户名查询数据库
+        String username = loginVo.getUsername();
+        LambdaQueryWrapper<SysUser> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(SysUser::getUsername,username);
+        SysUser sysUser = sysUserService.getOne(wrapper);
+
+        //3 用户信息是否存在
+        if(sysUser == null) {
+            throw new GuiguException(201,"用户不存在");
+        }
+
+        //4 判断密码
+        //数据库存密码（MD5）
+        String password_db = sysUser.getPassword();
+        //获取输入的密码
+        String password_input = MD5.encrypt(loginVo.getPassword());
+        if(!password_db.equals(password_input)) {
+            throw new GuiguException(201,"密码错误");
+        }
+
+        //5 判断用户是否被禁用  1 可用 0 禁用
+        if(sysUser.getStatus().intValue()==0) {
+            throw new GuiguException(201,"用户已经被禁用");
+        }
+
+        //6 使用jwt根据用户id和用户名称生成token字符串
+        String token = JwtHelper.createToken(sysUser.getId(), sysUser.getUsername());
+        //7 返回
+        Map<String,Object> map = new HashMap<>();
+        map.put("token",token);
         return Result.ok(map);
     }
 
